e7faa6bd21b6a92b0cc9f54ca3a25ec015be8212
Deepak Rai <Deepakrai767622@gmail.com>
2022-01-07 23:23:02 +0530

    sda
    dsa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./scss/style.scss";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+import HomeSection from "./components/HomeSection";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Wireframe from "./components/projects/Wireframe";
+import Ui_ux from "./components/projects/Ui_ux";
+import Reactjs from "./components/projects/Reactjs";
+import Jquery from "./components/projects/Jquery";
+import Javascript from "./components/projects/Javascript";
+import Front from "./components/projects/Front";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ScrollToTop>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomeSection />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/projects/wireframe" element={<Wireframe />} />
+            <Route path="/projects/ui_ux" element={<Ui_ux />} />
+            <Route path="/projects/react" element={<Reactjs />} />
+            <Route path="/projects/jquery" element={<Jquery />} />
+            <Route path="/projects/javascript" element={<Javascript />} />
+            <Route path="/projects/front" element={<Front />} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </Router>
     </div>
   );
 }
