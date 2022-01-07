0fa719a352f1bacec93358cdcd979e783bf454d9
Deepak Rai <Deepakrai767622@gmail.com>
2022-01-07 23:36:29 +0530

    asdas
    dsacxz

--- src/App.js
+++ src/App.js
@@ -12,27 +12,24 @@ import Reactjs from "./components/projects/Reactjs";
 import Jquery from "./components/projects/Jquery";
 import Javascript from "./components/projects/Javascript";
 import Front from "./components/projects/Front";
-import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <ScrollToTop>
-          <Header />
-          <Routes>
-            <Route path="/" element={<HomeSection />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/projects/wireframe" element={<Wireframe />} />
-            <Route path="/projects/ui_ux" element={<Ui_ux />} />
-            <Route path="/projects/react" element={<Reactjs />} />
-            <Route path="/projects/jquery" element={<Jquery />} />
-            <Route path="/projects/javascript" element={<Javascript />} />
-            <Route path="/projects/front" element={<Front />} />
-          </Routes>
-          <Footer />
-        </ScrollToTop>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomeSection />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects/wireframe" element={<Wireframe />} />
+          <Route path="/projects/ui_ux" element={<Ui_ux />} />
+          <Route path="/projects/react" element={<Reactjs />} />
+          <Route path="/projects/jquery" element={<Jquery />} />
+          <Route path="/projects/javascript" element={<Javascript />} />
+          <Route path="/projects/front" element={<Front />} />
+        </Routes>
+        <Footer />
       </Router>
     </div>
   );
